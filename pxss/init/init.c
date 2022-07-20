3014801cdc8aa855dce762da57cf9b07410b983f
Christian Rendina <christian.rendina@gmail.com>
2022-07-20 23:25:17 +0200

    And so it begins...

---
+++ pxss/init/init.c
@@ -0,0 +1,12 @@
+#include "pch.h"
+
+NTSTATUS
+__cdecl
+_main(IN INT argc,
+      IN PCHAR argv[],
+      IN PCHAR envp[],
+      IN ULONG DebugFlag)
+{
+    while (1);
+    return STATUS_SUCCESS;
+}
---
+++ pxss/init/pch.h
@@ -0,0 +1,26 @@
+#ifndef FAKESMSS_H
+#define FAKESMSS_H
+
+/* Native Headers */
+#define WIN32_NO_STATUS
+#include <windef.h>
+#include <winbase.h>
+#include <winreg.h>
+
+#define NTOS_MODE_USER
+#include <ndk/iofuncs.h>
+#include <ndk/obfuncs.h>
+#include <ndk/rtlfuncs.h>
+#include <ndk/cmfuncs.h>
+#include <ndk/exfuncs.h>
+#include <ndk/mmfuncs.h>
+#include <ndk/psfuncs.h>
+#include <ndk/lpcfuncs.h>
+#include <ndk/setypes.h>
+#include <ndk/umfuncs.h>
+#include <ndk/kefuncs.h>
+
+#include <ntstrsafe.h>
+
+#endif
+
