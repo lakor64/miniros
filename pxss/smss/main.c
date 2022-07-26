e0902522e165f50ca20bd565f79fffff38a34f4c
Christian Rendina <christian.rendina@gmail.com>
2022-07-27 01:36:10 +0200

    Prioritize smss process

--- pxss/smss/main.c
+++ pxss/smss/main.c
@@ -7,8 +7,25 @@ _main(IN INT argc,
       IN PCHAR envp[],
       IN ULONG DebugFlag)
 {
+    NTSTATUS Status;
+    KPRIORITY SetBasePriority;
+
     DPRINT1("smss init start\n");
 
+    /* Make us critical */
+    RtlSetProcessIsCritical(TRUE, NULL, FALSE);
+    RtlSetThreadIsCritical(TRUE, NULL, FALSE);
+
+    /* Raise our priority */
+    SetBasePriority = 11;
+    Status = NtSetInformationProcess(NtCurrentProcess(),
+                                     ProcessBasePriority,
+                                     (PVOID)&SetBasePriority,
+                                     sizeof(SetBasePriority));
+    ASSERT(NT_SUCCESS(Status));
+
+
     while (1);
-    return STATUS_SUCCESS;
+
+    return STATUS_SUCCESS; /* should NEVER happend */
 }
