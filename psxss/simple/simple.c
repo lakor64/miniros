44ab61372b8980b51af9b77930b37bcc1915e365
Christian Rendina <christian.rendina@gmail.com>
2022-08-19 06:23:00 +0200

    Add simple.exe entry and add a simple DPRINT1 for checking if simple bootup works

--- psxss/simple/simple.c
+++ psxss/simple/simple.c
@@ -1,15 +1,31 @@
-/* TODO: this will be removed */
-typedef const char* LPCSTR;
-typedef const void* LPCVOID;
-typedef unsigned long ULONG;
-typedef long LONG_PTR;
-typedef long LONG;
-typedef int INT;
+/* Native Headers */
+#define WIN32_NO_STATUS
+#include <windef.h>
+#include <winbase.h>
+#include <winreg.h>
+
+#define NTOS_MODE_USER
+#include <ndk/iofuncs.h>
+#include <ndk/obfuncs.h>
+#include <ndk/rtlfuncs.h>
+#include <ndk/cmfuncs.h>
+#include <ndk/exfuncs.h>
+#include <ndk/mmfuncs.h>
+#include <ndk/psfuncs.h>
+#include <ndk/lpcfuncs.h>
+#include <ndk/setypes.h>
+#include <ndk/umfuncs.h>
+#include <ndk/kefuncs.h>
+
+#include <ntstrsafe.h>
+
+#include <reactos/debug.h>
 
 #include <nt/syscalls.h>
 
 void _start()
 {
-    const char msg[] = "Hello from POSIX userland!";
-	sys_write(1, msg, sizeof(msg)-1);
+    DPRINT1("simple app start");
+    //const char msg[] = "Hello from POSIX userland!";
+	//sys_write(1, msg, sizeof(msg)-1);
 }
